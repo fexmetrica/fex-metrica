7708d98a3128e718ce73e7c0e95d02a901eee70e
filipporss <frossi@ucsd.edu>
2015-01-10 09:54:10 -0800

    Add list of files in contents.m

--- fexSDK/Contents.m
+++ fexSDK/Contents.m
@@ -1,3 +1,101 @@
 % FEXSDK
 %
-% Files
+% Files:
+% 
+% fexSDK/src/fexc.m
+% 
+% UI
+% 
+% fexSDK/src/ui/fexaboutg.m
+% fexSDK/src/ui/fexmetrica.m
+% 
+% UTIL
+% 
+% fexSDK/src/util/fex_balance.m
+% fexSDK/src/util/fex_bandpass.m
+% fexSDK/src/util/fex_box2linidx.m
+% fexSDK/src/util/fex_combo2.m
+% fexSDK/src/util/fex_facetproc.m
+% fexSDK/src/util/fex_falsepositive.m
+% fexSDK/src/util/fex_gauss2.m
+% fexSDK/src/util/fex_getcolors.m
+% fexSDK/src/util/fex_getmatrix.m
+% fexSDK/src/util/fex_imputil.m
+% fexSDK/src/util/fex_interpolate.m
+% fexSDK/src/util/fex_jsonparser.m
+% fexSDK/src/util/fex_kernel.m
+% fexSDK/src/util/fex_mwavelet.m
+% fexSDK/src/util/fex_nfoldscv.m
+% fexSDK/src/util/fex_normalize.m
+% fexSDK/src/util/fex_reallign.m
+% fexSDK/src/util/fex_reallign2.m
+% fexSDK/src/util/fex_robustimport.m
+% fexSDK/src/util/fex_strtime.m
+% fexSDK/src/util/fex_timeroi.m
+% fexSDK/src/util/fex_tsearch1.m
+% fexSDK/src/util/fex_whiteningt.m
+% fexSDK/src/util/fex_zcoord.m
+% fexSDK/src/util/fexdesignc.m
+% fexSDK/src/util/feximportdg.m
+% 
+% VIEWER 
+% 
+% fexSDK/src/viewer/fexw_filtplot.m
+% fexSDK/src/viewer/fexw_overlayui.m
+% fexSDK/src/viewer/fexw_pie.m
+% fexSDK/src/viewer/fexw_saveui.m
+% fexSDK/src/viewer/fexw_selectui.m
+% fexSDK/src/viewer/fexw_streamerui.m
+% fexSDK/src/viewer/fexw_summaryplot.m
+% fexSDK/src/viewer/fexw_timeplot.m
+% fexSDK/src/viewer/fexwdrawmaskui.m
+% fexSDK/src/viewer/fexwhdr.m
+% fexSDK/src/viewer/fexwimg.m
+% fexSDK/src/viewer/fexwoverlay.m
+% fexSDK/src/viewer/fexwsearchg.m
+% fexSDK/src/viewer/streamernotesui.m
+%     
+% SAMPLES
+% 
+% fexSDK/samples/fexmetrica_examples.m    
+%     
+% EXTERNAL
+% 
+% fexSDK/external/append_pdfs/append_pdfs.m
+% fexSDK/external/append_pdfs/ghostscript.m
+% fexSDK/external/append_pdfs/user_string.m
+% fexSDK/external/cm_and_cb_utilities/cbfreeze.m
+% fexSDK/external/cm_and_cb_utilities/cbhandle.m  
+%     
+%     
+% SHARED FILES
+%     
+% fexSDK/shared/eviewerhdrs.xlsx
+% fexSDK/shared/fexchannels.txt
+% fexSDK/shared/fexheaders.mat
+% fexSDK/shared/fexwmetadata.mat
+% fexSDK/shared/movie_placeholder.jpg
+% fexSDK/shared/template_01.fxw
+% fexSDK/shared/template_02.fxw
+% fexSDK/shared/template_03.fxw
+% fexSDK/shared/template_04.fxw
+% fexSDK/shared/template_anger.fxw
+% fexSDK/shared/template_contempt.fxw
+% fexSDK/shared/template_disgust.fxw
+% fexSDK/shared/template_fear.fxw
+% fexSDK/shared/template_joy.fxw
+% fexSDK/shared/template_neutral.fxw
+% fexSDK/shared/template_sadness.fxw
+% fexSDK/shared/template_surprise.fxw 
+%     
+% CPP files
+%
+% fexSDK/src/facet/cpp/linux/fexface.cpp
+% fexSDK/src/facet/cpp/linux/fexfacet.cpp
+% fexSDK/src/facet/cpp/linux/tools.cpp
+% fexSDK/src/facet/cpp/linux/config.hpp
+% fexSDK/src/facet/cpp/linux/tools.hpp
+% fexSDK/src/facet/cpp/osx/fexfacetexec.cpp
+% fexSDK/src/facet/cpp/osx/tools.cpp         
+% fexSDK/src/facet/cpp/osx/config.hpp
+% fexSDK/src/facet/cpp/osx/tools.hpp
