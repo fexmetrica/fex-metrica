715c1e32730d6e1d5560f061cdd3448eb4e7b2cd
Luke Chang <lukejchang@gmail.com>
2015-02-27 10:20:03 -0700

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -19,7 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-matlab_src_dir = os.path.abspath('..')  # MATLAB
+matlab_src_dir = os.path.abspath('../fexSDK/src')  # MATLAB
 primary_domain = 'mat'
 
 # -- General configuration ------------------------------------------------
